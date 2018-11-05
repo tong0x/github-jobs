6298d012a70c7bf09aea29efcca74582f7a87bcc
Tong Pow <guitong35@yahoo.com>
2018-11-05 09:02:17 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+import requests
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def postings():
+    url = 'https://jobs.github.com/positions.json'
+    r = requests.get(url).json()
+    jobs = []
+    for json in r:
+        curr_job = {
+            'description': json['description'],
+            'location': json['location'],
+            'title': json['title'],
+            'type': json['type'],
+            'company': json['company'],
+            'link': json['url']
+        }
+        jobs.append(curr_job)
+    return render_template('postings.html', jobs=jobs)
+
+# Endpoint for search results
+@app.route('/results', methods=['POST'])
+def results():
+    url = 'https://jobs.github.com/positions.json'
+    title = request.form['title']
+    location = request.form['location']
+    print(title, location)
+    r = requests.get(url).json()
+    jobs = []
+    for json in r:
+        print(json['title'], json['location'])
+        if (title.lower() in json['title'].lower() or json['title'].lower() in title.lower()) and (location.lower() in json['location'].lower() or json['location'].lower() in location.lower()):
+            curr_job = {
+                'description': json['description'],
+                'location': json['location'],
+                'title': json['title'],
+                'type': json['type'],
+                'company': json['company'],
+                'link': json['url']
+            }
+            jobs.append(curr_job)
+    return render_template('postings.html', jobs=jobs)  # title=title, location=location)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
